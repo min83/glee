b23088a0569a45bcc1d6856632875c06a11258e4
min83 <minerwa83@gmail.com>
2020-11-25 22:00:42 +0200

    added product added design sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
 $(function() {
+
    $('.top-slider__inner').slick({
       dots: true,
       arrows: false,
@@ -6,5 +7,21 @@ $(function() {
       autoplay: true,
       autoplaySpeed: 2000
    });
+
    
+   var mixer = mixitup('.product__content', {
+      selectors: {
+         control: '.product__btn'
+      }
+
+   });
+
+   var mixer = mixitup('.design__content', {
+      selectors:{
+         control: '.design__btn'
+      }
+
+   });
+
+  
 });
